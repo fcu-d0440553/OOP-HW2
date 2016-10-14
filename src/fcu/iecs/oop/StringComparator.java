7629e4cfdc8514d2ea04c0370f7bd439560762dd
user <user@FD05>
2016-10-14 10:02:12 +0800

    新增Break,Continue 修改StringComparator

--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -7,14 +7,21 @@ public class StringComparator {
 	public static void main(String[] args) {
 		
 		Scanner StringComparator = new Scanner(System.in);
-		System.out.printf("Enter a String1:");
-		String Str1 = StringComparator.next();
-		System.out.printf("Enter a String2:");
-		String Str2 = StringComparator.next();
-		if(Str1.equalsIgnoreCase(Str2))
+		
+		String Str1,Str2;
+		
+		boolean i;
+		
+		do{
+			System.out.printf("Enter a String1:");
+			Str1 = StringComparator.next();
+			System.out.printf("Enter a String2:");
+			Str2 = StringComparator.next();
+			System.out.println("The two strings are not the same.");
+			i=Str1.equalsIgnoreCase(Str2);
+		}while(!i);
+			
 			System.out.printf("The two strings are the same.");
-		else
-			System.out.printf("The two strings are not the same.");
 	}
 
 }
